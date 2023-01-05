e858184c757af1b1a261344c77a688682089c3a9
Meek <meek10x@gmail.com>
2023-01-04 21:28:06 -0600

    [feat] present and future cli commands

---
+++ analytica/present.py
@@ -0,0 +1,19 @@
+import sys
+from decimal import Decimal
+
+from analytica.investing import time_value_money
+
+
+def main():
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        print("Usage: present <future_value> <interest_rate> <time>")
+        sys.exit(1)
+
+    future_value, interest_rate, time = sys.argv[1], sys.argv[2], sys.argv[3]
+    calculate = time_value_money.TimeValueOfMoney()
+    fv = calculate.present_value_of_money(future_value, interest_rate, time)  # type: ignore
+    print(Decimal(fv).quantize(Decimal("0.01"), rounding="ROUND_HALF_UP"))
+
+
+if __name__ == "__main__":
+    main()
