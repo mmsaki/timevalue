50b83e028ada802f99a1c65a11d7af826ebd2322
Analytica (Dev) <meek10x@gmail.com>
2023-01-05 05:19:54 -0600

    [fix] reorder args order and update growning annuity calc

--- analytica/investing/annuity.py
+++ analytica/investing/annuity.py
@@ -108,12 +108,12 @@ class Annuity:
 
     # Future value of growing annuity
     def future_value_of_growing_annuity(
-        self, present_value: int, interest_rate: int, growth_rate: int, time: int
+        self, cash: int, interest_rate: int, time: int, growth_rate: int
     ) -> float:
         """Calculate the future value of growing annuity.
-        fvga = pva * ((1 + gr) / (r - gr)) * ((1 + r) ** t - 1)
+        fvga = c * ((1+r)**t - (1+gr)**t) / (r - gr)
         Args:
-            present_value (float): The present value of the investment.
+            cash (float): The cash value of the first payment.
             interest_rate (float): The interest rate of the investment.
             growth_rate (float): The growth rate of the investment.
             time (int): The time period of the investment.
@@ -121,20 +121,20 @@ class Annuity:
                 float: The future value of growing annuity.
         """
         return float(
-            Decimal(str(present_value))
+            Decimal(str(cash))
             * (
-                (1 + Decimal(str(growth_rate)))
-                / (Decimal(str(interest_rate)) - Decimal(str(growth_rate)))
+                (1 + Decimal(str(interest_rate))) ** Decimal(str(time))
+                - (1 + Decimal(str(growth_rate))) ** Decimal(str(time))
             )
-            * ((1 + Decimal(str(interest_rate))) ** Decimal(str(time)) - 1)
+            / (Decimal(str(interest_rate)) - Decimal(str(growth_rate)))
         )
 
     # Present value of growing annuity
     def present_value_of_growing_annuity(
-        self, future_value: int, interest_rate: int, growth_rate: int, time: int
+        self, first_payment: int, interest_rate: int, time: int, growth_rate: int
     ) -> float:
         """Calculate the present value of growing annuity.
-        pvga = fv * (r - gr) / ((1 + gr) * ((1 + r) ** t - 1))
+        pvga = p / (r - gr) * (1- ((1+gr)/(1+r))**t)
         Args:
             future_value (float): The future value of the investment.
             interest_rate (float): The interest rate of the investment.
@@ -144,17 +144,17 @@ class Annuity:
                 float: The present value of growing annuity.
         """
         return float(
-            Decimal(str(future_value))
-            * (Decimal(str(interest_rate)) - Decimal(str(growth_rate)))
-            / (
-                (1 + Decimal(str(growth_rate)))
-                * ((1 + Decimal(str(interest_rate))) ** Decimal(str(time)) - 1)
-            )
+            Decimal(str(first_payment))
+            / (Decimal(str(interest_rate)) - Decimal(str(growth_rate)))
+            * (
+                1 - ((1 + Decimal(str(growth_rate))) / (1 + Decimal(str(interest_rate))))
+                ** Decimal(str(time))
         )
+    )
 
     # Future value of growing annuity due
     def future_value_of_growing_annuity_due(
-        self, present_value: int, interest_rate: int, growth_rate: int, time: int
+        self, present_value: int, interest_rate: int, time: int, growth_rate: int
     ) -> float:
         """Calculate the future value of growing annuity due.
         fvgad = pva * ((1 + gr) / (r - gr)) * ((1 + r) ** t - 1) * (1 + r)
@@ -178,7 +178,7 @@ class Annuity:
 
     # Present value of growing annuity due
     def present_value_of_growing_annuity_due(
-        self, future_value: int, interest_rate: int, growth_rate: int, time: int
+        self, future_value: int, interest_rate: int, time: int, growth_rate: int
     ) -> float:
         """Calculate the present value of growing annuity due.
         pvgad = fv * (r - gr) / ((1 + gr) * ((1 + r) ** t - 1) * (1 + r))
@@ -280,7 +280,7 @@ class Annuity:
 
     # Future value of growing ordinary annuity
     def future_value_of_growing_ordinary_annuity(
-        self, present_value: int, interest_rate: int, growth_rate: int, time: int
+        self, present_value: int, interest_rate: int, time: int, growth_rate: int
     ) -> float:
         """Calculate the future value of growing ordinary annuity.
         fvgoad = pva * ((1 + r) ** t - 1) / (r - gr) * (1 + gr)
@@ -301,7 +301,7 @@ class Annuity:
 
     # Present value of growing ordinary annuity
     def present_value_of_growing_ordinary_annuity(
-        self, future_value: int, interest_rate: int, growth_rate: int, time: int
+        self, future_value: int, interest_rate: int, time: int, growth_rate: int
     ) -> float:
         """Calculate the present value of growing ordinary annuity.
         pvgoad = fv * (r - gr) / ((1 + r) ** t - 1) * (1 + gr)
