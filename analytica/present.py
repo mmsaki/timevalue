a26e01369a5587d7f37dd2f9bc36fe61645c86c7
Analytica (Dev) <meek10x@gmail.com>
2023-01-05 05:30:41 -0600

    [fix] flake8 spacing

--- analytica/present.py
+++ analytica/present.py
@@ -2,15 +2,18 @@ import argparse
 from decimal import Decimal
 from analytica.investing import time_value_money
 
-parser = argparse.ArgumentParser(description="Calculate present value of money.")
+parser = argparse.ArgumentParser(
+    description="Calculate present value of money.")
 parser.add_argument("future_value", type=float, help="future value")
 parser.add_argument("interest_rate", type=float, help="interest rate")
 parser.add_argument("time", type=int, help="time")
 
+
 def main():
     args = parser.parse_args()
     calculate = time_value_money.TimeValueOfMoney()
-    fv = calculate.present_value_of_money(args.future_value, args.interest_rate, args.time)  # type: ignore
+    fv = calculate.present_value_of_money(
+        args.future_value, args.interest_rate, args.time)  # type: ignore
     print(Decimal(fv).quantize(Decimal("0.01"), rounding="ROUND_HALF_UP"))
 
 
