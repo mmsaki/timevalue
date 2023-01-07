27b7f6d448875dc9c0d964e197598af7d27425d3
Meek <meek10x@gmail.com>
2023-01-07 00:42:00 -0600

    [pep] fixed linting

--- analytica/__main__.py
+++ analytica/__main__.py
@@ -1,19 +1,19 @@
 import argparse
 from decimal import Decimal
-from analytica.investing import time_value_money
 
-parser = argparse.ArgumentParser(
-    description="Calculate present value of money.")
-parser.add_argument("future_value", type=float, help="future value")
-parser.add_argument("interest_rate", type=float, help="interest rate")
-parser.add_argument("time", type=int, help="time")
+from analytica.investing import time_value_money
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Calculate present value of money.")
+    parser.add_argument("future_value", type=float, help="future value")
+    parser.add_argument("interest_rate", type=float, help="interest rate")
+    parser.add_argument("time", type=int, help="time")
     args = parser.parse_args()
     calculate = time_value_money.TimeValueOfMoney()
     fv = calculate.present_value_of_money(
-        args.future_value, args.interest_rate, args.time)  # type: ignore
+        args.future_value, args.interest_rate, args.time
+    )
     print(Decimal(fv).quantize(Decimal("0.01"), rounding="ROUND_HALF_UP"))
 
 
