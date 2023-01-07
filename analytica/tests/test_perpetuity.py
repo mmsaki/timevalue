cd42c445fff4e3f0fe8562a7254e27adec10e916
Meek <meek10x@gmail.com>
2023-01-07 07:17:22 -0600

    [tests] perpetuty due test

--- analytica/tests/test_perpetuity.py
+++ analytica/tests/test_perpetuity.py
@@ -6,11 +6,27 @@ def test_present_value_of_perpetuity():
     pv = c / r
     """
     # Arrange
-    cash_flow = 2
-    discount_rate = 0.05
-    expected = 2 / 0.05
+    cash_flow = 500
+    discount_rate = 0.04
+    expected = 500 / 0.04
     # Actual
     actual = perpetuity.Perpetuity().present_value_of_perpetuity(
+        cash_flow, discount_rate  # type: ignore
+    )
+    # Assert
+    assert round(actual, 2) == round(expected, 2)
+
+
+def test_present_value_of_perpetuity_due():
+    """Test the future value of perpetuity.
+    pv = c / r * (1 + r)
+    """
+    # Arrange
+    cash_flow = 500
+    discount_rate = 0.04
+    expected = 500 / 0.04 * (1 + 0.04)
+    # Actual
+    actual = perpetuity.Perpetuity().present_value_of_perpetuity_due(
         cash_flow, discount_rate  # type: ignore
     )
     # Assert
